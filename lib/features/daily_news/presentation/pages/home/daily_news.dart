a188da5a8d8995bfb666f7c8caa43a53697d1b91
adipati27ma <adipati.cs.samsung@gmail.com>
2024-03-28 16:22:15 +0700

    feat: UI additional done

--- lib/features/daily_news/presentation/pages/home/daily_news.dart
+++ lib/features/daily_news/presentation/pages/home/daily_news.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_news_app_cleanarch/features/daily_news/domain/entities/article.dart';
 import 'package:flutter_news_app_cleanarch/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
 import 'package:flutter_news_app_cleanarch/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart';
 import 'package:flutter_news_app_cleanarch/features/daily_news/presentation/widgets/article_tile.dart';
@@ -11,44 +12,63 @@ class DailyNews extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _buildAppBar(),
+      appBar: _buildAppbar(context),
       body: _buildBody(),
     );
   }
 
-  _buildAppBar() {
+  _buildAppbar(BuildContext context) {
     return AppBar(
       title: const Text(
-        'Daily News Jancoq',
+        'Daily News',
         style: TextStyle(color: Colors.black),
       ),
+      actions: [
+        GestureDetector(
+          onTap: () => _onShowSavedArticlesViewTapped(context),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 14),
+            child: Icon(Icons.bookmark, color: Colors.black),
+          ),
+        ),
+      ],
     );
   }
 
   _buildBody() {
     return BlocBuilder<RemoteArticlesBloc, RemoteArticleState>(
-      builder: (context, state) {
+      builder: (_, state) {
         if (state is RemoteArticlesLoading) {
-          return const Center(
-            child: CupertinoActivityIndicator(),
-          );
-        } else if (state is RemoteArticlesDone) {
+          return const Center(child: CupertinoActivityIndicator());
+        }
+
+        if (state is RemoteArticlesError) {
+          return const Center(child: Icon(Icons.refresh));
+        }
+
+        if (state is RemoteArticlesDone) {
           return ListView.builder(
-            itemCount: state.articles!.length,
             itemBuilder: (context, index) {
-              final article = state.articles![index];
               return ArticleWidget(
-                article: article,
+                article: state.articles![index],
+                onArticlePressed: (article) =>
+                    _onArticlePressed(context, article),
               );
             },
-          );
-        } else if (state is RemoteArticlesError) {
-          return Center(
-            child: Text(state.error.toString()),
+            itemCount: state.articles!.length,
           );
         }
-        return const SizedBox.shrink();
+
+        return const SizedBox();
       },
     );
+  }
+
+  void _onArticlePressed(BuildContext context, ArticleEntity article) {
+    Navigator.pushNamed(context, '/ArticleDetails', arguments: article);
+  }
+
+  void _onShowSavedArticlesViewTapped(BuildContext context) {
+    Navigator.pushNamed(context, '/SavedArticles');
   }
 }
--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,3 +1,4 @@
+import 'package:get_it/get_it.dart';
 import 'package:flutter_news_app_cleanarch/features/daily_news/data/data_sources/local/app_database.dart';
 import 'package:flutter_news_app_cleanarch/features/daily_news/data/repository/article_repository_impl.dart';
 import 'package:flutter_news_app_cleanarch/features/daily_news/domain/repository/article_repository.dart';
@@ -5,8 +6,8 @@ import 'package:flutter_news_app_cleanarch/features/daily_news/domain/usecases/g
 import 'package:flutter_news_app_cleanarch/features/daily_news/domain/usecases/get_saved_article.dart';
 import 'package:flutter_news_app_cleanarch/features/daily_news/domain/usecases/remove_article.dart';
 import 'package:flutter_news_app_cleanarch/features/daily_news/domain/usecases/save_article.dart';
+import 'package:flutter_news_app_cleanarch/features/daily_news/presentation/bloc/article/local/local_article_bloc.dart';
 import 'package:flutter_news_app_cleanarch/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
-import 'package:get_it/get_it.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_news_app_cleanarch/features/daily_news/data/data_sources/remote/news_api_service.dart';
 
@@ -55,4 +56,7 @@ Future<void> initializeDependencies() async {
   // BLoC (Presentation Layer, Factory)
   sl.registerFactory<RemoteArticlesBloc>(
       () => RemoteArticlesBloc(sl<GetArticleUseCase>()));
+
+  sl.registerFactory<LocalArticleBloc>(
+      () => LocalArticleBloc(sl(), sl(), sl()));
 }
